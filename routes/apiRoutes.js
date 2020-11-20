0911383ad2104d0b5eeb971737278c1c3692b0fd
alysonbennett <alysonbennett@gmai.com>
2020-11-19 23:15:38 -0500

    adding dependencies and routing to htmlRoutes.js file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,3 +2,12 @@
 const fs = require("fs");
 const notesData = require("..db/db.json");
 
+//Routing
+module.exports = function(app) {
+
+    //API GET Requests
+    app.get("/api/notes", function (req, res) {
+        res.json(notesData);
+    });
+
+}
---
+++ routes/htmlRoutes.js
@@ -0,0 +1,16 @@
+//Dependencies
+const path = require("path");
+
+//Routing
+module.exports = function(app) {
+
+    //Route to index page
+    app.get("/notes", function (req, rest) {
+        rest.sendFile(path.join(__dirname, "../public/index.html"));
+    });
+
+    //Route to notes page
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/notes.html"));
+    });
+};
