36b25329d17faa7500b6a0f64536e659e94204e1
alysonbennett <alysonbennett@gmai.com>
2020-11-24 18:25:15 -0500

    apiRotes working

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,17 +1,58 @@
 //Required modules
+// const notesData = require("../db/db.json");
 const fs = require("fs");
-const notesData = require("../db/db.json");
+const path = require("path");
+const DB_PATH = path.join(__dirname, '../db/db.json');
+
+// const writeFile = (path, data) => fs.writeFileSync(path, JSON.stringify(data))
+// const readFile = (path, enc) => JSON.parse(fs.readFileSync(path, enc))
+
+// const writeDB = data => writeFile(DB_PATH, data)
+// const readDB = () => readFile(DB_PATH, "utf8")
+
+const __DB__ = "../db/db.json";
+class DB {
+    constructor(dir) {
+        this.dir = path.join(__dirname, dir);
+        this.data = this.readDB();
+    }
+
+    writeFile(path, data) { fs.writeFileSync(path, JSON.stringify(data)) }
+    readFile(path, enc) { return JSON.parse(fs.readFileSync(path, enc)) }
+
+    writeDB(data) { this.writeFile(this.dir, data) }
+    readDB() { return this.readFile(this.dir, "utf8") }
+
+    add(item) {
+        item.id = 1;
+
+        this.data.push(item)
+        this.writeDB(this.data)
+    }
+
+    findAll() {
+        return this.data
+    }
+}
 
 //Routing
-module.exports = function(app) {
+module.exports = function (app) {
 
     //API GET Request, displays all notes
     app.get("/api/notes", function (req, res) {
-        res.json(notesData);
+        // const notesData = readDB();
+
+        res.json(new DB(__DB__).findAll());
     });
 
     //Create new notes
-    app.post("/api/notes", function(req, res) {
+    app.post("/api/notes", function (req, res) {
+        // const notesData = readDB();
+        // notesData.push(req.body);
+        // writeDB(notesData);
 
+        new DB(__DB__).add(req.body)
+
+        res.sendStatus(200);
     })
 }
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,14 +3,14 @@ const path = require("path");
 
 //Routing
 module.exports = function(app) {
+    //Route to notes page
+    app.get("/notes", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/notes.html"));
+    });
 
     //Route to index page
     app.get("*", function (req, rest) {
         rest.sendFile(path.join(__dirname, "../public/index.html"));
     });
 
-    //Route to notes page
-    app.get("/notes", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/notes.html"));
-    });
 };
